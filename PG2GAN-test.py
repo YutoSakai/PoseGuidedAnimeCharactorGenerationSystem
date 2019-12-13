a2270565927f832e86ab0eb70a302d8915508c6c
Yuto Sakai <yutosakai0106z@gmail.com>
2019-12-13 15:02:30 +0900

    PG2GAN-testを実装 keypoints_from_images.pyのreturn_Pb_Ib()をreturn_Ib_Pb()とリターンの順に関数名を変更

--- PG2GAN-test.py
+++ PG2GAN-test.py
@@ -1,15 +1,7 @@
 from __future__ import print_function
-import argparse
-import random
-import torch.nn as nn
-import torch.backends.cudnn as cudnn
-import torch.optim as optim
 import torch.utils.data
 import torchvision.utils as vutils
 import torch
-from torch.autograd import Variable
-
-from Dataset import MyDataset
 import PG2GAN
 import keypoints_from_images
 import cv2
@@ -20,9 +12,9 @@ target_image_path = "imagedir/target_image.png"
 
 def test(Ia, Pb):
     netG1 = PG2GAN.NetG1()
-    netG1.load_state_dict(torch.load('outpth/netG1_epoch_%d.pth'))
+    netG1.load_state_dict(torch.load('outpth/netG1_epoch_%d.pth'))  # %dを最新の数字にしなきゃ
     netG2 = PG2GAN.NetG2()
-    netG2.load_state_dict(torch.load('outpth/netG2_epoch_%d.pth'))
+    netG2.load_state_dict(torch.load('outpth/netG2_epoch_%d.pth'))  # %dを最新の数字にしなきゃ
 
     G1_x = torch.cat((Ia, Pb), 1)
     G1_out = netG1(G1_x)
