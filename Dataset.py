5830e00c25dfded9bcb7eff38ad631d19f9b7d92
Yuto Sakai <yutosakai0106z@gmail.com>
2019-12-24 15:59:58 +0900

    [:, [2, 1, 0], :, :]ってな感じでRGBを入れ替えたらうまく行ったかも

--- Dataset.py
+++ Dataset.py
@@ -47,7 +47,7 @@ class MyDataset(torch.utils.data.Dataset):
             same_id_paths = [i for i in glob.glob(str(data_dir) + "/" + str(id_str) + "*.jpg", recursive=True) if i != img_path]
             for same_id_path in same_id_paths:
                 self.pair.append((same_id_path, img_path))
-            if i == 1:
+            if i == 100:
                 break
 
     def __len__(self):
