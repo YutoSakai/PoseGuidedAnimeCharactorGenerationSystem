7478681aaf8d429f1e851cf30a49b51a479c6624
Yuto Sakai <yutosakai0106z@gmail.com>
2020-02-26 13:33:40 +0900

    画像サイズ出力

--- keypoints_from_images.py
+++ keypoints_from_images.py
@@ -53,10 +53,10 @@ class Keypoints_from_images:
             return_keypoints_images = []
             for keypoints_image in keypoints_images:
                 resize_keypoints_image = cv2.resize(keypoints_image, (int(keypoints_image.shape[0] / 8), int(keypoints_image.shape[1] / 8)))
-                height, width = resize_keypoints_image.shape[:2]
-                    # 取得結果（幅，高さ，チャンネル数，depth）を表示
-                print("width: " + str(width))
-                print("height: " + str(height))
+                # height, width = resize_keypoints_image.shape[:2]
+                #     # 取得結果（幅，高さ，チャンネル数，depth）を表示
+                # print("width: " + str(width))
+                # print("height: " + str(height))
                 return_keypoints_images.append(resize_keypoints_image)
             return return_image, return_keypoints_images
 
